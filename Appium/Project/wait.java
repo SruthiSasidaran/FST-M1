43bb862288696f151d18d61a0e0d9ba909a08d8f
SruthiSasidaran <S.Sruthi@ibm.com>
2025-09-26 14:31:41 +0530

    Add files via upload

---
+++ Appium/Project/wait.java
@@ -0,0 +1,28 @@
+package Project;
+
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.ExpectedCondition;
+
+public class wait {
+
+	public static void until11(ExpectedCondition<WebElement> visibilityOfElementLocated) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	public static void until1(ExpectedCondition<WebElement> visibilityOfElementLocated) {
+		// TODO Auto-generated method stub
+		
+	}
+
+
+		// TODO Auto-generated method stub
+		
+	
+
+	public static void until(ExpectedCondition<WebElement> visibilityOfElementLocated) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
